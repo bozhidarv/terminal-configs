7de6e1c6d36cd660ac5f163def1752ad55aad2c9
Bozhidar Videnov <bozhidar.videnov@sciant.com>
2022-09-07 09:39:36 +0300

    init

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+Import-Module posh-git
+C:\Users\bozhidar.videnov\AppData\Local\Programs\oh-my-posh\bin\oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\clean-detailed.omp.json" | Invoke-Expression
